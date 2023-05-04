314306c2679111e051a5ca56a9cb299e66af7518
jcads <rafaelcadorna@pm.me>
2023-05-04 13:17:40 +0800

    Add 'add_allocations' property

--- src/pterodactyl/body/update_server_build.cr
+++ src/pterodactyl/body/update_server_build.cr
@@ -5,5 +5,6 @@ module Pterodactyl::Body
     property allocation : Int32
     property limits : Models::Limits
     property feature_limits : Models::FeatureLimits
+    property add_allocations : Array(Int32) = []
   end
 end

05f6c2579648ffbb55f3c669a8e0eaea67d2d925
jcads <rafaelcadorna@pm.me>
2023-05-04 13:30:14 +0800

    Add element type

--- src/pterodactyl/body/update_server_build.cr
+++ src/pterodactyl/body/update_server_build.cr
@@ -5,6 +5,6 @@ module Pterodactyl::Body
     property allocation : Int32
     property limits : Models::Limits
     property feature_limits : Models::FeatureLimits
-    property add_allocations : Array(Int32) = []
+    property add_allocations : Array(Int32) = [] of Int32
   end
 end
